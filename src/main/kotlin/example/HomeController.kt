27500bcb811eaf65e130319abd66666b5ce066e0
Hidetake Iwata <int128@gmail.com>
2017-08-24 17:28:26 +0900

    Fix logging system

--- src/main/kotlin/example/HomeController.kt
+++ src/main/kotlin/example/HomeController.kt
@@ -1,14 +1,20 @@
 package example
 
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import java.time.LocalDateTime
+
+val log = LoggerFactory.getLogger(HomeController::class.java)
 
 @Controller
 class HomeController {
     @GetMapping("/hello")
-    fun index(model: Model): String {
+    fun index(model: Model, @RequestHeader(HttpHeaders.USER_AGENT) userAgent: String): String {
+        log.debug("Got request from {}", userAgent)
         model.addAttribute("currentDateTime", LocalDateTime.now())
         return "hello"
     }
