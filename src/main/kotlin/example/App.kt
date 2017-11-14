1d3f410b9579726e7bb752f148cb5388281df1a1
Hidetake Iwata <int128@gmail.com>
2017-11-14 20:53:24 +0900

    Bump to Spring Boot 2.0.0.M6

--- src/main/kotlin/example/App.kt
+++ src/main/kotlin/example/App.kt
@@ -1,7 +1,12 @@
 package example
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 open class App : SpringBootServletInitializer()
+
+fun main(args: Array<String>) {
+    SpringApplication.run(App::class.java, *args)
+}
